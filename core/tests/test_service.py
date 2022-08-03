44f6f2277aee0189d837ae8043a5070fd0746748
tarekbadrshalaan <tarekbadrshalaan@gmail.com>
2022-08-03 15:17:13 +0200

    add taskrun obj

--- core/tests/test_service.py
+++ core/tests/test_service.py
@@ -1,40 +1,44 @@
 from cdevents.core import event_type
 import pytest
 
-from cdevents.core.events import Events
+from cdevents.core.service import Service, ServiceType
 
 @pytest.mark.unit
 def test_service_deployed():
-    event = Events().create_service_event(event_type.ServiceDeployedEventV1, envid="_envid", name="_name", version="_version", data={"service": "_service"})
-    assert event is not None
-    assert event._attributes["type"] == event_type.ServiceDeployedEventV1
-    assert event._attributes["extensions"] == {"serviceenvid": "_envid", "servicename": "_name", "serviceversion": "_version"}
-    assert event.data == {"service": "_service"}
+    service = Service(service_type=ServiceType.ServiceDeployedEventV1, envid="_envid", name="_name", version="_version")
+    service_event = service.create_event(data={"key1": "value1"})
+    assert service_event is not None
+    assert service_event._attributes["type"] == ServiceType.ServiceDeployedEventV1.value
+    assert service_event._attributes["extensions"] == {"serviceenvid": "_envid", "servicename": "_name", "serviceversion": "_version"}
+    assert service_event.data == {"key1": "value1"}
 
 
 @pytest.mark.unit
 def test_service_upgraded():
-    event = Events().create_service_event(event_type.ServiceUpgradedEventV1, envid="_envid", name="_name", version="_version", data={"service": "_service"})
-    assert event is not None
-    assert event._attributes["type"] == event_type.ServiceUpgradedEventV1
-    assert event._attributes["extensions"] == {"serviceenvid": "_envid", "servicename": "_name", "serviceversion": "_version"}
-    assert event.data == {"service": "_service"}
+    service = Service(service_type=ServiceType.ServiceUpgradedEventV1, envid="_envid", name="_name", version="_version")
+    service_event = service.create_event(data={"key1": "value1"})
+    assert service_event is not None
+    assert service_event._attributes["type"] == ServiceType.ServiceUpgradedEventV1.value
+    assert service_event._attributes["extensions"] == {"serviceenvid": "_envid", "servicename": "_name", "serviceversion": "_version"}
+    assert service_event.data == {"key1": "value1"}
 
 
 @pytest.mark.unit
 def test_service_rolledback():
-    event = Events().create_service_event(event_type.ServiceRolledbackEventV1, envid="_envid", name="_name", version="_version", data={"service": "_service"})
-    assert event is not None
-    assert event._attributes["type"] == event_type.ServiceRolledbackEventV1
-    assert event._attributes["extensions"] == {"serviceenvid": "_envid", "servicename": "_name", "serviceversion": "_version"}
-    assert event.data == {"service": "_service"}
+    service = Service(service_type=ServiceType.ServiceRolledbackEventV1, envid="_envid", name="_name", version="_version")
+    service_event = service.create_event(data={"key1": "value1"})
+    assert service_event is not None
+    assert service_event._attributes["type"] == ServiceType.ServiceRolledbackEventV1.value
+    assert service_event._attributes["extensions"] == {"serviceenvid": "_envid", "servicename": "_name", "serviceversion": "_version"}
+    assert service_event.data == {"key1": "value1"}
 
 
 @pytest.mark.unit
 def test_service_removed():
-    event = Events().create_service_event(event_type.ServiceRemovedEventV1, envid="_envid", name="_name", version="_version", data={"service": "_service"})
-    assert event is not None
-    assert event._attributes["type"] == event_type.ServiceRemovedEventV1
-    assert event._attributes["extensions"] == {"serviceenvid": "_envid", "servicename": "_name", "serviceversion": "_version"}
-    assert event.data == {"service": "_service"}
+    service = Service(service_type=ServiceType.ServiceRemovedEventV1, envid="_envid", name="_name", version="_version")
+    service_event = service.create_event(data={"key1": "value1"})
+    assert service_event is not None
+    assert service_event._attributes["type"] == ServiceType.ServiceRemovedEventV1.value
+    assert service_event._attributes["extensions"] == {"serviceenvid": "_envid", "servicename": "_name", "serviceversion": "_version"}
+    assert service_event.data == {"key1": "value1"}
 
