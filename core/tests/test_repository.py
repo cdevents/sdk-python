f53567a0846afc6b55cb6812b883f6490bab3ea6
tarekbadrshalaan <tarekbadrshalaan@gmail.com>
2022-08-03 14:57:36 +0200

    add repository obj

--- core/tests/test_repository.py
+++ core/tests/test_repository.py
@@ -1,28 +1,34 @@
 from cdevents.core import event_type
 import pytest
 
-from cdevents.core.events import Events
+from cdevents.core.repository import Repository, RepositoryType
 
 @pytest.mark.unit
 def test_repository_created():
-    event = Events().create_repository_event(event_type.RepositoryCreatedEventV1, id="_id", name="_name", url="_url", data={"repository": "_repository"})
-    assert event is not None
-    assert event._attributes["type"] == event_type.RepositoryCreatedEventV1
-    assert event._attributes["extensions"] ==  {"repositoryid": "_id", "repositoryname": "_name", "repositoryurl": "_url"}
-    assert event.data == {"repository": "_repository"}
+    repository = Repository(repository_type=RepositoryType.RepositoryCreatedEventV1, id="_id", name="_name", url="_url")
+    repository_event = repository.create_event(data={"key1": "value1"})
+    assert repository_event is not None
+    assert repository_event._attributes["type"] == RepositoryType.RepositoryCreatedEventV1.value
+    assert repository_event._attributes["extensions"] == {"repositoryid": "_id", "repositoryname": "_name", "repositoryurl": "_url"}
+    assert repository_event.data == {"key1": "value1"}
+
 
 @pytest.mark.unit
 def test_repository_modified():
-    event = Events().create_repository_event(event_type.RepositoryModifiedEventV1, id="_id", name="_name", url="_url", data={"repository": "_repository"})
-    assert event is not None
-    assert event._attributes["type"] == event_type.RepositoryModifiedEventV1
-    assert event._attributes["extensions"] ==  {"repositoryid": "_id", "repositoryname": "_name", "repositoryurl": "_url"}
-    assert event.data == {"repository": "_repository"}
+    repository = Repository(repository_type=RepositoryType.RepositoryModifiedEventV1, id="_id", name="_name", url="_url")
+    repository_event = repository.create_event(data={"key1": "value1"})
+    assert repository_event is not None
+    assert repository_event._attributes["type"] == RepositoryType.RepositoryModifiedEventV1.value
+    assert repository_event._attributes["extensions"] == {"repositoryid": "_id", "repositoryname": "_name", "repositoryurl": "_url"}
+    assert repository_event.data == {"key1": "value1"}
+
 
 @pytest.mark.unit
 def test_repository_deleted():
-    event = Events().create_repository_event(event_type.RepositoryDeletedEventV1, id="_id", name="_name", url="_url", data={"repository": "_repository"})
-    assert event is not None
-    assert event._attributes["type"] == event_type.RepositoryDeletedEventV1
-    assert event._attributes["extensions"] ==  {"repositoryid": "_id", "repositoryname": "_name", "repositoryurl": "_url"}
-    assert event.data == {"repository": "_repository"}
+    repository = Repository(repository_type=RepositoryType.RepositoryDeletedEventV1, id="_id", name="_name", url="_url")
+    repository_event = repository.create_event(data={"key1": "value1"})
+    assert repository_event is not None
+    assert repository_event._attributes["type"] == RepositoryType.RepositoryDeletedEventV1.value
+    assert repository_event._attributes["extensions"] == {"repositoryid": "_id", "repositoryname": "_name", "repositoryurl": "_url"}
+    assert repository_event.data == {"key1": "value1"}
+
