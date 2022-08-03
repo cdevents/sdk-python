f2a8cd9087b66734dee71a0569b68bc2a1ce3d5d
tarekbadrshalaan <tarekbadrshalaan@gmail.com>
2022-08-03 14:46:39 +0200

    add PipelineRun obj

--- core/tests/test_pipelinerun.py
+++ core/tests/test_pipelinerun.py
@@ -1,29 +1,30 @@
-from pickle import NONE
-from cdevents.core import event_type
 import pytest
 
-from cdevents.core.events import Events
+from cdevents.core.pipelinerun import Pipelinerun, PipelinerunType
 
 @pytest.mark.unit
 def test_pipelinerun_started():
-    event = Events().create_pipelinerun_event(event_type.PipelineRunStartedEventV1, id="_id", name="_name", status="_status", url="_url", errors="_errors", data={"pipelinerun": "_pipelinerun"})
-    assert event is not None
-    assert event._attributes["type"] == event_type.PipelineRunStartedEventV1
-    assert event._attributes["extensions"] ==  {"pipelinerunid": "_id", "pipelinerunname": "_name", "pipelinerunstatus": "_status", "pipelinerunurl": "_url", "pipelinerunerrors": "_errors"}
-    assert event.data == {"pipelinerun": "_pipelinerun"}
+    pipelinerun = Pipelinerun(pipelinerun_type=PipelinerunType.PipelineRunStartedEventV1, id="_id", name="_name", status="_status", url="_url", errors="_errors")
+    pipelinerun_event = pipelinerun.create_event(data={"key1": "value1"})
+    assert pipelinerun_event is not None
+    assert pipelinerun_event._attributes["type"] == PipelinerunType.PipelineRunStartedEventV1.value
+    assert pipelinerun_event._attributes["extensions"] == {"pipelinerunid": "_id", "pipelinerunname": "_name", "pipelinerunstatus": "_status", "pipelinerunurl": "_url", "pipelinerunerrors": "_errors"}
+    assert pipelinerun_event.data == {"key1": "value1"}
 
 @pytest.mark.unit
 def test_pipelinerun_finished():
-    event = Events().create_pipelinerun_event(event_type.PipelineRunFinishedEventV1, id="_id", name="_name", status="_status", url="_url", errors="_errors", data={"pipelinerun": "_pipelinerun"})
-    assert event is not None
-    assert event._attributes["type"] == event_type.PipelineRunFinishedEventV1
-    assert event._attributes["extensions"] ==  {"pipelinerunid": "_id", "pipelinerunname": "_name", "pipelinerunstatus": "_status", "pipelinerunurl": "_url", "pipelinerunerrors": "_errors"}
-    assert event.data == {"pipelinerun": "_pipelinerun"}
+    pipelinerun = Pipelinerun(pipelinerun_type=PipelinerunType.PipelineRunFinishedEventV1, id="_id", name="_name", status="_status", url="_url", errors="_errors")
+    pipelinerun_event = pipelinerun.create_event(data={"key1": "value1"})
+    assert pipelinerun_event is not None
+    assert pipelinerun_event._attributes["type"] == PipelinerunType.PipelineRunFinishedEventV1.value
+    assert pipelinerun_event._attributes["extensions"] == {"pipelinerunid": "_id", "pipelinerunname": "_name", "pipelinerunstatus": "_status", "pipelinerunurl": "_url", "pipelinerunerrors": "_errors"}
+    assert pipelinerun_event.data == {"key1": "value1"}
 
 @pytest.mark.unit
 def test_pipelinerun_queued():
-    event = Events().create_pipelinerun_event(event_type.PipelineRunQueuedEventV1, id="_id", name="_name", status="_status", url="_url", errors="_errors", data={"pipelinerun": "_pipelinerun"})
-    assert event is not None
-    assert event._attributes["type"] == event_type.PipelineRunQueuedEventV1
-    assert event._attributes["extensions"] ==  {"pipelinerunid": "_id", "pipelinerunname": "_name", "pipelinerunstatus": "_status", "pipelinerunurl": "_url", "pipelinerunerrors": "_errors"}
-    assert event.data == {"pipelinerun": "_pipelinerun"}
+    pipelinerun = Pipelinerun(pipelinerun_type=PipelinerunType.PipelineRunQueuedEventV1, id="_id", name="_name", status="_status", url="_url", errors="_errors")
+    pipelinerun_event = pipelinerun.create_event(data={"key1": "value1"})
+    assert pipelinerun_event is not None
+    assert pipelinerun_event._attributes["type"] == PipelinerunType.PipelineRunQueuedEventV1.value
+    assert pipelinerun_event._attributes["extensions"] == {"pipelinerunid": "_id", "pipelinerunname": "_name", "pipelinerunstatus": "_status", "pipelinerunurl": "_url", "pipelinerunerrors": "_errors"}
+    assert pipelinerun_event.data == {"key1": "value1"}
